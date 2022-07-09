b93b5d3841361b45d5c2f5a045d8f90113f87f4f
kaho2580 <Katsuma8888@gmail.com>
2022-07-09 18:57:41 +0900

    [add] フロントエンドの追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('homes/index')->with(['posts' => $post->getPaginateByLimit()]);
     }
     
     public function show(Post $post)
