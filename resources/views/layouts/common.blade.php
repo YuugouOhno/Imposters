9a2e87c3907dd8661dcc5a81f89e9fb33e3e338c
yoktak <yokotaku327@gmail.com>
2022-07-19 01:47:22 +0900

    [branch yokochi02更新用コミット（訂正箇所なし）]

--- resources/views/layouts/common.blade.php
+++ resources/views/layouts/common.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!--<meta http-equiv="X-UA-Compatible" content="IE=edge">-->
         <link rel="stylesheet" href="{{ asset('/CSS/common.css') }}">
     </head>
     <body>
@@ -12,10 +12,10 @@
     <header>
             <nav class="gnav">
                 <ul class="menu">
-                    <li><a href="/">[トップページ]</a></li>
-                    <li><a href="/diary/create">[一日日記]</a></li>
-                    <li><a href="/math">[電卓]</a></li>
-                    <li><a href="/translator">[翻訳]</a></li>
+                    <li><a href="/">トップページ</a></li>
+                    <li><a href="/diary/create">一日日記</a></li>
+                    <li><a href="/math">電卓</a></li>
+                    <li><a href="/translator">翻訳</a></li>
                 </ul>
             </nav>
     </header>
