77e87555599ca811295267c6346b41293a05a0d4
Hiroki1238 <c14h14n3nao3s@gmail.com>
2022-09-02 12:52:31 +0900

    [add]読み上げ機能のテストを追加

--- resources/views/calculator/index.blade.php
+++ resources/views/calculator/index.blade.php
@@ -20,6 +20,37 @@
   <a href='/calculator/4'>電卓4</a>
   <a href='/calculator/5'>電卓5</a>
   <a href='/calculator/6'>電卓6</a>
+  <a onclick="speech()">音声テスト</a>
+  
+  <script>
+  function speech(){
+    if ('speechSynthesis' in window) {
+      console.log("オンクリック");
+
+    // 発言を設定 (必須)
+    const uttr = new SpeechSynthesisUtterance()
+
+    // テキストを設定 (必須)
+    uttr.text = 'いち、に、さん、よん、ご'
+
+    // 言語を設定
+    uttr.lang = "ja-JP"
+
+    // 速度を設定
+    uttr.rate = 1
+
+    // 高さを設定
+    uttr.pitch = 2
+
+    // 音量を設定
+    uttr.volume = 1
+
+    // 発言を再生 (必須)
+    window.speechSynthesis.speak(uttr)
+
+ }
+ }
+  </script>
 </body>
 </html>
 @endsection
