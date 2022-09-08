a61ada880cb3eda290ec45ca35d8a287152d572d
yoktak <yokotaku327@gmail.com>
2022-09-08 09:31:56 +0900

    [modified]電卓2,4フロント

--- resources/views/calculator/calc2.blade.php
+++ resources/views/calculator/calc2.blade.php
@@ -3,13 +3,19 @@
 
 @extends('layouts.app')
 @section('head')
- <link rel="preconnect" href="https://fonts.googleapis.com">
- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
- <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('/css/calc2.css')}}">
-@endsection
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('/css/calc2.css')}}">
+@endsection('head')
+
 @section('content')
-    <calc2-component></calc2-component>
+    <div class='calc2'>
+        <calc2-component></calc2-component>
+        <div class='image'>
+            <img class="img" src="/css/img/piero1.png">
+        </div>
+    </div>
 @endsection
 
 @endsection
